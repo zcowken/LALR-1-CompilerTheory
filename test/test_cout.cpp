1bfee8b7a36956d247547664e78855668f4f3277
zcowken <2601436406@qq.com>
2024-11-04 23:46:39 +0800

    杂项

---
+++ test/test_cout.cpp
@@ -0,0 +1,13 @@
+//
+// Created by zken on 2024/10/23.
+//
+#include <iostream>
+using namespace  std;
+
+int main()
+{
+    cout<<"ab "<<"\t"<<"cd"<<endl;
+    cout<<std::left;
+    cout<<"ab "<<"\t"<<"cd"<<endl;
+    return 0;
+}
